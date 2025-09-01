39de21e8dfa0f5aad1a3c695cd3ca91dc5328a90
Hector <hector.david.1987@gmail.com>
2025-09-01 15:59:56 +0100

    adding mistral example code

---
+++ mistralExample/mistral.py
@@ -0,0 +1,18 @@
+# Synchronous Example
+from mistralai import Mistral
+import os
+
+
+with Mistral(
+    api_key=os.getenv("MISTRAL_API_KEY", ""),
+) as mistral:
+
+    res = mistral.chat.complete(model="mistral-small-latest", messages=[
+        {
+            "content": "Who is the best French painter? Answer in one short sentence.",
+            "role": "user",
+        },
+    ], stream=False)
+
+    # Handle response
+    print(res)
